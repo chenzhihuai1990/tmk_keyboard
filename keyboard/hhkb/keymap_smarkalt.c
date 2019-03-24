30f167bb62d94dd71a1552286da50f32aa45085f
Zhihuai Chen <chenzhihuai1990@gmail.com>
2019-03-24 17:31:41 +0800

    add toggle_space to cancel smartalt

--- keyboard/hhkb/keymap_smarkalt.c
+++ keyboard/hhkb/keymap_smarkalt.c
@@ -3,12 +3,11 @@
  */
 #include "keymap_common.h"
 #define SPACEFN_LAYER 3
+#define SMARTALT_LAYER 5
 enum function_id {
-    DUAL_ALT,
-    ALT_LAYER_KEY,
+    SMARTALT_ACTIVATION,
+    SMARTALT_NAGVIATIONS_FUNCTIONS,
 };
-//define SCTL FN6
-/*define SCTL LCTL*/
 
 #ifdef KEYMAP_SECTION_ENABLE
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
@@ -33,7 +32,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN4,             \
-                LGUI, FN0,          SPC,                FN2,RGUI),
+                LGUI, FN0,          SPC,                FN2,RALT),
 
     /* Layer 1: SpaceFn Layer
      * ,-----------------------------------------------------------.
@@ -71,7 +70,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,HOME,UP,  END, TRNS,TRNS,      \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PGUP,LEFT,DOWN,RGHT,PENT,           \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PGDN,TRNS,TRNS,TRNS,TRNS,           \
-                TRNS,TRNS,          TRNS,               TRNS,TRNS),
+                TRNS,LALT,          TRNS,               TRNS,TRNS),
 
     /* Layer 3: SpaceFn Mode 
      * ,-----------------------------------------------------------.
@@ -111,7 +110,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
            TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,APP ,TRNS,TRNS,           \
                 TRNS,TRNS,          FN1,               TRNS,TRNS),
 
-    /* Layer 5: LAlt Fn Mode 
+    /* Layer 5: SmartAlt Mode 
      * ,-----------------------------------------------------------.
      * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
@@ -127,8 +126,8 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP(TRNS,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,   \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN20,FN20, FN20, TRNS,TRNS,TRNS,TRNS,     \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN20,FN20,FN20,FN20,TRNS,TRNS,TRNS,           \
-           TRNS,TRNS,TRNS,TRNS,TRNS,SPC, FN20,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,           \
-                TRNS,TRNS,          TRNS,               TRNS,TRNS),
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, FN20,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,           \
+                TRNS,TRNS,          FN10,               RALT,TRNS),
 };
 
 
@@ -144,51 +143,68 @@ const action_t fn_actions[] PROGMEM = {
     [1]  = ACTION_DEFAULT_LAYER_TOGGLE(1),
     [2]  = ACTION_LAYER_TAP_TOGGLE(2),//AltFn
     [3]  = ACTION_LAYER_TAP_KEY(3, KC_SPC),//SpaceFn
-    [4]  = ACTION_LAYER_MOMENTARY(4),//SapceFn
+    [4]  = ACTION_LAYER_MOMENTARY(4),//SpaceFn
 
-    [0]  = ACTION_FUNCTION(DUAL_ALT),
-
-    [20] = ACTION_FUNCTION(ALT_LAYER_KEY),
+    [0]  = ACTION_FUNCTION(SMARTALT_ACTIVATION),
+    [10]  = ACTION_FUNCTION_TAP(SMARTALT_SPACE),
+    [20] = ACTION_FUNCTION(SMARTALT_NAGVIATION),
 };
 
 /*
  * user defined action function
+ * TO-TEST: Alt + Shift will switch input method, thus we can't use alt + shift + arrow to select chars ?
+ * (TODO)SOLUTION: when press shift will just add_mods(LSHIFT) but not send it until other key is press.
  */
+
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-    if (record->event.pressed) dprint("-P"); else dprint("R");
+    if (record->event.pressed) dprint("P"); else dprint("R");
     dprintf("%d", record->tap.count);
     if (record->tap.interrupted) dprint("i");
     dprint("\n");
-    dprintf("real: %u\n", get_mods());
-    dprintf("weak: %u\n", get_weak_mods());
 
     switch (id) {
-        case DUAL_ALT:
+        case SMARTALT_ACTIVATION:
             if (record->event.pressed) {
                 add_weak_mods(MOD_BIT(KC_LALT));
-                layer_invert(5);
+                layer_on(SMARTALT_LAYER);
             }else
             {
-                send_keyboard_report();
-                layer_invert(5);
+                send_keyboard_report();// send previous event: D(KC_LALT)
+                layer_off(SMARTALT_LAYER);
                 del_weak_mods(MOD_BIT(KC_LALT));
                 send_keyboard_report();
             }
             break;
 
-        case ALT_LAYER_KEY:
+        case SMARTALT_NAGVIATION_FUNCTIONS:
             if (record->event.pressed) {
-                dprintf("key at position %" PRIu16 " is pressed\n", record->event.key);
                 del_weak_mods(MOD_BIT(KC_LALT));
                 uint8_t keycode = keymap_key_to_keycode(SPACEFN_LAYER, record->event.key);
                 add_key(keycode);
                 send_keyboard_report();
             } else {
-                //todo switch
                 uint8_t keycode = keymap_key_to_keycode(SPACEFN_LAYER, record->event.key);
                 del_key(keycode);
                 send_keyboard_report();
+            }
+            break;
+        case SMARTALT_SPACE:
+            if (record->event.pressed) {
+                if (record->tap.count > 0 && !record->tap.interrupted) {
+                    if (record->tap.interrupted) {
+                        layer_off(SMARTALT_LAYER);
+                    }
+                } else {
+                    layer_off(SMARTALT_LAYER);
+                }
+            } else {
+                if (record->tap.count > 0 && !(record->tap.interrupted)) {
+                    type_code(KC_SPC);
+                    record->tap.count = 0;
+                } else {
+                    layer_on(SMARTALT_LAYER);
+                }
             }
             break;
     }
