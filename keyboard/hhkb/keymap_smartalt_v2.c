cf209c74b3b4067e93afbd6d8c1b79b5799b041d
Zhihuai Chen <chenzhihuai1990@gmail.com>
2019-04-09 06:36:58 +0000

    add new scheme

--- keyboard/hhkb/keymap_smartalt_v2.c
+++ keyboard/hhkb/keymap_smartalt_v2.c
@@ -20,7 +20,6 @@ enum function_id {
     SMART_ALT_EXTRA_KEYS,   //functions key and naviation keys
     SMART_ALT_NORMAL,       //press other key except ijkl/hnuo after press smart alt
     SMART_ALT_PENDING_MODS, //press shift etc. after press smart alt
-    SMART_ALT_SPACE,        //Alt+Space
     SMART_ALT_ESC,          //activate function keys (use function keys instead of number keys)
     SMART_ALT_SCLN,         //Alt+uiohjkln
 };
@@ -47,7 +46,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN6,             \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN5,             \
                 LGUI, FN3,          SPC,                FN4,RALT),
 
     /* Layer 1: SpaceFn Mode
@@ -66,7 +65,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN5,             \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN6,             \
                 LGUI,LALT,          FN2,                FN4,RGUI),
 
     /* Layer 2: SpaceFn Layer 
@@ -88,22 +87,22 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
            TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,           \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
-    /* Layer 3: SmartAlt Layer 
+    /* Layer 3: Smart Alt Layer 
      * ,-----------------------------------------------------------.
-     * |LCK| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |FT | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
      * |     |   |   |   |   |   |   |Hom|Up |End|   |   |   |     |
      * |-----------------------------------------------------------|
      * |      |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
      * |-----------------------------------------------------------|
-     * |        |   |   |   |   |Spc|PgD|   |   |   |   |      |   |
+     * |        |   |   |   |   |   |PgD|   |   |   |   |      |   |
      * `-----------------------------------------------------------'
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
     KEYMAP( FN7,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,FN20,  \
            FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN20,FN20,FN20,FN21,FN21,FN21,FN21,       \
-           FN21,FN21,FN21,FN21,FN21,FN21,FN20,FN20,FN20,FN20,FN21,FN21,FN21,            \
+           FN21,FN21,FN21,FN21,FN21,FN21,FN20,FN20,FN20,FN20, FN8,FN21,FN21,            \
            FN22,FN21,FN21,FN21,FN21,FN21,FN20,FN21,FN21,FN21,FN21,FN21,FN21,            \
                 FN21,FN21,          FN10,               FN4,FN21),
 
@@ -126,7 +125,8 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
            TRNS,TRNS,TRNS,TRNS,TRNS,SPC,    N,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,           \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),*/
 
-    KEYMAP( ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,INS,DEL,   \
+    /* convenient for totalcmd */
+    KEYMAP( ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,INS,DEL,   \ 
            TRNS,   1,   2,   3,   4,   5,TRNS,HOME,  UP, END, TRNS,MINS, EQL,TRNS,     \
            TRNS,   6,   7,   8,   9,   0,PGUP,LEFT,DOWN,RGHT,SCLN,TRNS,TRNS,           \
            TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,           \
@@ -147,11 +147,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      */
     KEYMAP(TRNS,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS,DEL,    \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   HOME,UP,  END, RBRC,BSPC,       \
-           LCTL,A,   S,   D,   F,   G,   H,   J,   PGUP,LEFT,DOWN,RGHT, ENT,            \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   PGDN,DOT, SLSH,RSFT,RCTL,            \
+           LCTL,A,   S,   D,   F,   G,   H,   J,   PGUP,LEFT,DOWN,RGHT,ENT,             \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   PGDN,DOT, SLSH,RSFT,RCTL,             \
                 LGUI, LALT,          SPC,                TRNS,RALT),
 
-    /* Layer 6: Media Layer 
+    /* Layer 6: Media Layer  (Actived be FN)
      * ,-----------------------------------------------------------.
      * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
      * |-----------------------------------------------------------|
@@ -164,6 +164,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |   |     |       SPCFN_EN        |     |   |
      *       `-------------------------------------------'
      */
+
     KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
            TRNS,PSCR,SLCK,PAUS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,      \
            TRNS,MPRV,MPLY,MNXT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,           \
@@ -204,15 +205,16 @@ const action_t fn_actions[] PROGMEM = {
     [1]  = ACTION_DEFAULT_LAYER_TOGGLE ( SPACEFN_MODE_LAYER        ) ,//Toggle SpaceFn Mode
     [2]  = ACTION_LAYER_TAP_KEY        ( SPACEFN_LAYER, KC_SPC     ) ,//SpaceFn Key
     [3]  = ACTION_FUNCTION             ( SMART_ALT                 ) ,
-    [4]  = ACTION_LAYER_TAP_TOGGLE     ( RALTFN_LAYER              ) ,//AltFn
+    [4]  = ACTION_LAYER_TAP_TOGGLE     ( RALTFN_LAYER              ) ,//Right-Alt Fn
 
-    [6]  = ACTION_LAYER_MOMENTARY      ( MEDIA_LAYER               ) ,//Media Layer
+    [6]  = ACTION_LAYER_MOMENTARY      ( MEDIA_LAYER               ) ,//Media Fn
 
     //following keys is available only if SMARTALT layer was activated
     [7]  = ACTION_FUNCTION             ( SMART_ALT_ESC             ) ,
+    [8]  = ACTION_FUNCTION_TAP         ( SMART_ALT_SCLN            ) ,
     [10] = ACTION_LAYER_TAP_KEY        ( ALT_SPACE_LAYER, KC_SPACE ) ,//Tapping send alt+space while hold acitivate complement layer (alt+space layer)
-    [20] = ACTION_FUNCTION             ( SMART_ALT_EXTRA_KEYS      ) ,//arrow keys and home/end/pgup/pgdown and F1-F12
-    [21] = ACTION_FUNCTION             ( SMART_ALT_NORMAL          ) ,//modifier LALT is sent with when it is pressed
+    [20] = ACTION_FUNCTION             ( SMART_ALT_EXTRA_KEYS      ) ,//navigation & function keys and insert, delete keys
+    [21] = ACTION_FUNCTION             ( SMART_ALT_NORMAL          ) ,//LAlt + {pressed key}
     [22] = ACTION_FUNCTION             ( SMART_ALT_PENDING_MODS    ) ,//when modifier(such as ctrl or shift) is pressed, just suspend themself untill other key is press.(avoid sending alt when you press them).
 };
 
@@ -228,13 +230,13 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
     dprint("\n");
 
     switch (id) {
-        case SMART_ALT:
+        case SMART_ALT://activate smartalt layer while pending `alt` to report;
             if (record->event.pressed) {
-                add_weak_mods(MOD_BIT(KC_LALT));
+                add_weak_mods(MOD_BIT(KC_LALT)); //pending `alt`
                 layer_invert(SMARTALT_LAYER);
             }else
             {
-                send_keyboard_report();// send previous event: D(KC_LALT)
+                send_keyboard_report(); // send previous event: D(KC_LALT)
                 layer_invert(SMARTALT_LAYER);
                 del_weak_mods(MOD_BIT(KC_LALT));
                 send_keyboard_report();
@@ -253,7 +255,7 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 send_keyboard_report();
             }
             break;
-        /*case SMART_ALT_SPACE:
+        case SMART_ALT_SCLN:
             if (record->event.pressed) {
                 if (record->tap.count > 0 && !record->tap.interrupted) {
                     if (record->tap.interrupted) {
@@ -264,21 +266,14 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 }
             } else {
                 if (record->tap.count > 0 && !(record->tap.interrupted)) {
-                    type_code(KC_SPC);
+                    type_code(KC_SCLN);
                     record->tap.count = 0;
                 } else {
                     layer_invert(SMARTALT_LAYER);
                 }
             }
-            break;*/
-        case SMART_ALT_ESC:
-            if (record->event.pressed) {
-                del_weak_mods(MOD_BIT(KC_LALT));
-            } else {
-                layer_invert(FUNCTION_LAYER);
-            }
             break;
-        case SMART_ALT_NORMAL:
+        case SMART_ALT_NORMAL: // send alt+{pressed key}
             if (record->event.pressed) {
                 add_weak_mods(MOD_BIT(KC_LALT));
                 uint8_t keycode = keymap_key_to_keycode(0, record->event.key);
@@ -288,6 +283,13 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 uint8_t keycode = keymap_key_to_keycode(0, record->event.key);
                 del_key(keycode);
                 send_keyboard_report();
+            }
+            break;
+        case SMART_ALT_ESC:
+            if (record->event.pressed) {
+                del_weak_mods(MOD_BIT(KC_LALT));
+            } else {
+                layer_invert(FUNCTION_LAYER);
             }
             break;
         case SMART_ALT_PENDING_MODS:
